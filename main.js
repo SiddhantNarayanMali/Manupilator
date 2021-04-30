01278136097a5f8354608520e4bf9d8c0988ab86
SiddhantNarayanMali <76616431+SiddhantNarayanMali@users.noreply.github.com>
2021-04-30 16:01:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+    Video = createCapture(VIDEO);
+    Video.size(550,550);
+    Ml5 = ml5.poseNet(Video,ModelL);
+    Ml5.on("pose" , posing);
+
+}
+
+function ModelL()
+{
+    console.log("Posenet Is Initiallized");
+}
+
+function posing(r)
+{
+    if (r.length > 0)
+    {
+        console.log(r)
+    }
+}
+
+function draw()
+{
+}
